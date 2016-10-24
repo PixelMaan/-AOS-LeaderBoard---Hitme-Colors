5277777ba8c6ccfe0c540dfeaa54db40dd8f752b
Vu Le <vu.lt@neo-lab.vn>
2016-10-24 17:08:30 +0700

    Update game screen

--- app/src/main/java/com/f5live/projtemplate/feature/home/presenter/HomeViewPresenter.java
+++
@@ -1,9 +0,0 @@
-package com.f5live.projtemplate.feature.home.presenter;
-
-/**
- * Copyright © 2016 Neo-Lab Co.,Ltd.
- * Created by VuLT on 24/10/2016.
- */
-
-public interface HomeViewPresenter {
-}
